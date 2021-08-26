8f1eee3d896cd44c0ba492679294bf0d59a30d00
ALEXX-VIV <mario.alex.vivsenco@gmail.com>
2021-08-26 17:11:59 +0200

    Boh

---
+++ js/app.js
@@ -0,0 +1,34 @@
+console.log('Vue ok', Vue);
+
+Vue.config.devtools = true;
+
+const app = new Vue({
+  el: '#app',
+  data:{
+    user: {
+      name: 'Nome Utente',
+      avatar: '_io',
+    },
+    contacts: [
+      {
+        name: 'Michele',
+        avatar: '_1',
+      },
+      {
+        name: 'Fabio',
+        avatar: '_2',
+      },
+      {
+        name: 'Samuele',
+        avatar: '_3',
+      },
+      {
+        name: 'Luisa',
+        avatar: '_4',
+      },
+    ],
+    link: '',
+  },
+  methods:{
+  },
+})
