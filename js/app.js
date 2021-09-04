913de1589086977599fa9e57b6b2077838508d04
Alexx-Viv <81645879+Alexx-Viv@users.noreply.github.com>
2021-09-04 22:14:55 +0200

    Add funcxtion to send message

--- js/app.js
+++ js/app.js
@@ -9,12 +9,38 @@ const app = new Vue({
     contactImage: '../img/avatar_1.jpg',
     contactName: 'Michele',
     contactIndex: 0,
+    searchContact: '',
+    userMessage: '',
+    timestamp: '',
   },
   methods: {
     changeContact(name, image, index) {
       this.contactName = name;
       this.contactImage = '.' + image;
       this.contactIndex = index;
+    },
+    addMessage(userMessage, contactIndex) {
+      const today = new Date();
+      const date =
+        today.getFullYear() +
+        '/' +
+        (today.getMonth() + 1) +
+        '/' +
+        today.getDate();
+      const time =
+        today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+      const dateTime = date + ' ' + time;
+      this.timestamp = dateTime;
+
+      const newMessage = {
+        date: this.timestamp,
+        message: userMessage,
+        status: 'sent',
+      };
+
+      this.data.contacts[contactIndex].messages.push(newMessage);
+
+      this.userMessage = '';
     },
   },
 });
